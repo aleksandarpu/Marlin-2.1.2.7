5d0fddbe40c6cfe6048fa2aacedd6aca6a88b908
aleksandarputic <aleksandarpu@gmail.com>
2026-02-24 09:05:47 +0100

    Z_MIN_PROBE_REPEATABILITY_TEST, MONITOR_DRIVER_STATUS,SQUARE_WAVE_STEPPING,TMC_DEBUG

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3100,7 +3100,7 @@
    * M912 - Clear stepper driver overtemperature pre-warn condition flag.
    * M122 - Report driver parameters (Requires TMC_DEBUG)
    */
-  //#define MONITOR_DRIVER_STATUS
+  #define MONITOR_DRIVER_STATUS
 
   #if ENABLED(MONITOR_DRIVER_STATUS)
     #define CURRENT_STEP_DOWN     50  // [mA]
@@ -3207,13 +3207,13 @@
   /**
    * Step on both rising and falling edge signals (as with a square wave).
    */
-  //#define SQUARE_WAVE_STEPPING
+  #define SQUARE_WAVE_STEPPING
 
   /**
    * Enable M122 debugging command for TMC stepper drivers.
    * M122 S0/1 will enable continuous reporting.
    */
-  //#define TMC_DEBUG
+  #define TMC_DEBUG
 
   /**
    * You can set your own advanced settings by filling in predefined functions.
